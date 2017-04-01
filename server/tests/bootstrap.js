c3640134e4fc7d697581b9c480e8384fedfb0c4d
Ingo Schommer <ingo@silverstripe.com>
2017-04-01 19:43:48 +1300

    Long stack traces

--- server/tests/bootstrap.js
+++ server/tests/bootstrap.js
@@ -1,3 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+  // Long stack traces
+  // See https://trace.js.org/
+  // See https://github.com/aws/aws-sdk-js/issues/1358
+  require('trace');
+  Error.stackTraceLimit = 100;
+}
+
 process.on('unhandledRejection', (reason, p) => {
   console.log(JSON.stringify(reason));
   console.log(JSON.stringify(p));
