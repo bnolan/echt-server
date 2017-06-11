62c7fa672228793171eb1d63488612da96d76597
Ben Nolan <bnolan@gmail.com>
2017-06-12 11:37:27 +1200

    Truncate uat.

---
+++ tasks/truncate-uat.js
@@ -0,0 +1,41 @@
+const dynamodbHelper = require('../helpers/dynamodb');
+const rekognitionHelper = require('../helpers/rekognition');
+const sleep = require('sleep-promise');
+
+if (process.argv.indexOf('YESIMSURE=1') < 0) {
+  throw new Error('You must pass YESIMSURE=1 as a parameter to truncate uat');
+}
+
+const stage = 'uat';
+global.ECHT_STAGE = stage;
+
+console.log('Empty Rekognition...');
+
+rekognitionHelper.emptyCollection(stage)
+  .then(() => {
+    console.log('dropUsers...');
+    return dynamodbHelper.dropUsers(stage);
+  })
+  .then(() => {
+    console.log('dropPhotos...');
+    return dynamodbHelper.dropPhotos(stage);
+  })
+  .then(() => {
+    console.log('dropFaces...');
+    return dynamodbHelper.dropFaces(stage);
+  })
+  .then(sleep(5000))
+  .then(() => {
+    console.log('createUsers...');
+    return dynamodbHelper.createUsers(stage);
+  })
+  .then(() => {
+    console.log('createPhotos...');
+    return dynamodbHelper.createPhotos(stage);
+  })
+  .then(() => {
+    console.log('createFaces...');
+    return dynamodbHelper.createFaces(stage);
+  }).then(() => {
+    console.log('Done');
+  });
