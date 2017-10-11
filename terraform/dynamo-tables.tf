72b5bea94a2a6573344945a961eacfaf73ac5683
Ben Nolan <bnolan@gmail.com>
2017-10-11 18:52:25 +1300

    Dynamodb tables

--- terraform/dynamo-tables.tf
+++ terraform/dynamo-tables.tf
@@ -35,3 +35,72 @@ resource "aws_dynamodb_table" "users-table" {
   #   Environment = "production"
   # }
 }
+
+resource "aws_dynamodb_table" "photos-table" {
+  name           = "echt.photos"
+  read_capacity  = 2
+  write_capacity = 1
+  hash_key       = "uuid"
+  range_key      = "userId"
+
+  attribute {
+    name = "uuid"
+    type = "S"
+  }
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+
+  attribute {
+    name = "createdAt"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "echt.photosByUserId"
+    hash_key           = "userId"
+    range_key          = "createdAt"
+    write_capacity     = 2
+    read_capacity      = 1
+    projection_type    = "ALL"
+    # non_key_attributes = ["UserId"]
+  }
+}
+
+resource "aws_dynamodb_table" "faces-table" {
+  name           = "echt.faces"
+  read_capacity  = 2
+  write_capacity = 1
+  hash_key       = "faceId"
+  range_key      = "userId"
+
+  attribute {
+    name = "faceId"
+    type = "S"
+  }
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "friends-table" {
+  name           = "echt.friends"
+  read_capacity  = 2
+  write_capacity = 1
+  hash_key       = "fromId"
+  range_key      = "toId"
+
+  attribute {
+    name = "fromId"
+    type = "S"
+  }
+
+  attribute {
+    name = "toId"
+    type = "S"
+  }
+}
